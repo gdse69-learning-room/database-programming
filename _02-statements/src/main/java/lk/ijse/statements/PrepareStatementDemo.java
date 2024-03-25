69d80b9a95f6c7d6f000da8ff447e20186be4da5
danujav <danujagreru@gmail.com>
2024-03-25 11:05:59 +0530

    preparestatement save

---
+++ _02-statements/src/main/java/lk/ijse/statements/PrepareStatementDemo.java
@@ -0,0 +1,51 @@
+package lk.ijse.statements;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class PrepareStatementDemo {
+    private static void saveCustomer(String id, String name, String address, String tel) throws SQLException {
+        String sql = "INSERT INTO customers VALUES(?, ?, ?, ?)";
+
+        try(Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Ijse@1234"
+        )) {
+            PreparedStatement pstm = connection.prepareStatement(sql);//return -> PrepareStatement
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            pstm.setObject(4, tel);
+
+            int affectedRows = pstm.executeUpdate();
+            if(affectedRows > 0)
+                System.out.println("customer saved!");
+            else
+                System.out.println("customer not saved!");
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input customer id: ");
+        String id = input.next();
+
+        System.out.print("Input new customer name: ");
+        String name = input.next();
+
+        System.out.print("Input new customer address: ");
+        String address = input.next();
+
+        System.out.print("Input new customer tel: ");
+        String tel = input.next();
+
+//        saveCustomer(id, name, address, tel);
+
+//        updateCustomer(id, name,  address, tel);
+    }
+}
