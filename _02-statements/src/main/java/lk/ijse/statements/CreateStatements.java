c3092e97a7a159ea944dc5f354e0fa8607742e07
danujav <danujagreru@gmail.com>
2024-03-25 09:55:13 +0530

    save, getAll and seachById done

---
+++ _02-statements/src/main/java/lk/ijse/statements/CreateStatements.java
@@ -0,0 +1,90 @@
+package lk.ijse.statements;
+
+import java.sql.*;
+
+public class CreateStatements {
+    private static void saveCustomer() throws ClassNotFoundException, SQLException {
+        /* since JDBC4.x not compulsory to load the Driver *//*
+        Class.forName("com.mysql.cj.jdbc.Driver");*/
+
+        String sql = "INSERT INTO customers VALUES('C005', 'Danushi', 'Galle', '0765481254')";
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Ijse@1234"
+        );
+        Statement statement = connection.createStatement();
+        int affectedRows = statement.executeUpdate(sql);
+
+        if (affectedRows > 0)
+            System.out.println("Customer saved!");
+        else
+            throw new RuntimeException("Customer not saved!");
+
+        connection.close();
+
+    }
+
+    private static void getAllCustomer() throws SQLException {
+        String sql = "SELECT * FROM customers";
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Ijse@1234"
+        );
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);    //customer table -> customer ResultSet
+
+        while (resultSet.next()) {
+//            String id = resultSet.getString(1);
+            String id = resultSet.getString("id");
+            String name = resultSet.getString("name");
+            String address = resultSet.getString("address");
+            String tel = resultSet.getString("tel");
+
+            System.out.println(id + " " + name + " " + address + " " + tel);
+        }
+
+        connection.close();
+    }
+
+    private static void searchCustomerById() throws SQLException {
+        String sql = "SELECT * FROM customers WHERE id = 'C001'";
+
+        try(Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Ijse@1234"
+        )) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);    //return type -> int OR ResultSet
+
+            if (resultSet.next()) {
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                String tel = resultSet.getString(4);
+
+                System.out.println(id + " " + name + " " + address + " " + tel);
+            } else {
+                System.out.println("OOPs! customer not found!");
+            }
+        }
+//        connection.close();   // No need
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        saveCustomer();
+
+//        getAllCustomer();
+
+//        searchCustomerById();
+
+//        deleteCustomer();
+
+//        updateCustomer();
+
+    }
+}
