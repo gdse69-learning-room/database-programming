1223ed754fb5e914aa2d039dac9836f2ee47e2a5
danujav <danujagreru@gmail.com>
2024-04-01 13:07:11 +0530

    getcustomer count

--- _04-supermarket/src/main/java/lk/ijse/supermarket/controller/DashboardFormController.java
+++ _04-supermarket/src/main/java/lk/ijse/supermarket/controller/DashboardFormController.java
@@ -3,6 +3,7 @@ package lk.ijse.supermarket.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -10,6 +11,21 @@ import java.io.IOException;
 
 public class DashboardFormController {
     public AnchorPane rootNode;
+    public Label lblCustomerCount;
+    private int customerCount;
+
+    public void initialize() {
+        customerCount = getCustomerCount();
+        setCustomerCount(customerCount);
+    }
+
+    private void setCustomerCount(int customerCount) {
+        lblCustomerCount.setText(String.valueOf(customerCount));
+    }
+
+    private int getCustomerCount() {
+        return 5;
+    }
 
     public void btnExitOnAction(ActionEvent actionEvent) throws IOException {
         AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
