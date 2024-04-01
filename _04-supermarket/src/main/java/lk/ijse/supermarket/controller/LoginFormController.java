7129bb25cf3268a25fc7f6020297f95cd760cb2b
danujav <danujagreru@gmail.com>
2024-04-01 11:06:42 +0530

    login form done

--- _04-supermarket/src/main/java/lk/ijse/supermarket/controller/LoginFormController.java
+++ _04-supermarket/src/main/java/lk/ijse/supermarket/controller/LoginFormController.java
@@ -4,11 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.supermarket.db.DbConnection;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginFormController {
     public TextField txtUserId;
@@ -16,9 +22,37 @@ public class LoginFormController {
     public AnchorPane rootNode;
 
     public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        String userId = txtUserId.getText();
+        String pw = txtPassword.getText();
 
+        try {
+            checkCredential(userId, pw);
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
+    private void checkCredential(String userId, String pw) throws SQLException {
+        String sql = "SELECT user_id, password FROM users WHERE user_id = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setObject(1, userId);
+
+        ResultSet resultSet = pstm.executeQuery();
+        if(resultSet.next()) {
+            String dbPw = resultSet.getString("password");
+
+            if(pw.equals(dbPw)) {
+                System.out.println("You can navigate to the dashboard");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "sorry! password is incorrect!").show();
+            }
+
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "sorry! user id can't be find!").show();
+        }
+    }
 
 
     public void linkRegistrationOnAction(ActionEvent actionEvent) throws IOException {
